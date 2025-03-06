0e2d7c0c5d96ef73636e093cd85e69af2978d060
Matthew Doyle <137286886+matthewjdoyle@users.noreply.github.com>
2025-03-06 15:55:40 +0000

    Create build_apps.sh

---
+++ build_apps.sh
@@ -0,0 +1,4 @@
+pyinstaller scripts/ideal_gas_2d.py --onefile --noconsole
+pyinstaller scripts/ideal_gas_3d.py --onefile --noconsole
+pyinstaller scripts/x-ray_attenuation.py --onefile --noconsole
+
